0ee5ad6529437f9a8f1388bd1a085ab838413d51
Ayşegül YANIK <aysegul_demirtas@hotmail.com.tr>
2021-03-15 21:30:28 +0300

    Web Service Applications
    1 server 1 consuming client and 1 mini heartbeat console app i

---
+++ tinyServiceSample_console/tinyServiceSample_console/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace tinyServiceSample_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<heartbeat>(s =>
+                    {
+                        s.ConstructUsing(heartbeat => new heartbeat());
+                        s.WhenStarted(heartbeat => heartbeat.Start());
+                        s.WhenStopped(heartbeat => heartbeat.Stop());
+                    });
+
+                x.RunAsLocalSystem();
+                x.SetServiceName("HeartbeatService");
+                x.SetDisplayName("Heartbeat Service");
+                x.SetDescription("This is a simple heartbeat service imitated from youtube video : by Tim Corey");
+
+
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
---
+++ tinyServiceSample_console/tinyServiceSample_console/heartbeat.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace tinyServiceSample_console
+{
+    public class heartbeat
+    {
+        private readonly Timer _timer;
+
+        public heartbeat()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += TimerElapsed;
+
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            string[] lines = new string[] { DateTime.Now.ToString() };
+
+            //insert a valid path ant create there a text file named as heartbeat
+            File.AppendAllLines(@"C:\heartbeat.txt",lines);
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
